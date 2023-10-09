856007c80703ab40918daa99e35b3d938e66548b
Taya <95543492+tayamaken@users.noreply.github.com>
2023-10-09 16:58:23 +0300

    Update ulits.py

--- ulits.py
+++ ulits.py
@@ -61,7 +61,6 @@ def generate_face(img, mask, prompt, seed, guidance_scale = 7.5, num_samples = 4
   return images
 
 def merge(original, face, faces_info, size_of_cropped, sec_num):
-  face = resize_convert(face, size_of_cropped)  # только для первого лица
   x = faces_info[0, 0]
   y = faces_info[0, 1]
   original[y-sec_num:y-sec_num+face.shape[0], x-sec_num:x-sec_num+face.shape[1]] = face
