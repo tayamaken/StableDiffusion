43525e3a5b57c33e53b747a249dc3460e461e1d8
Воложева Таисия <t.volozheva@mail.ru>
2023-11-07 13:44:49 +0300

    adding gradient function to ulits

--- ulits.py
+++ ulits.py
@@ -3,8 +3,9 @@ import PIL
 import requests
 import os
 import torch
-import numpy
+import numpy as np
 import cv2 #opencv itself
+import matplotlib.pyplot as plt
 from PIL import Image
 from io import BytesIO
 from diffusers import StableDiffusionInpaintPipeline
@@ -18,7 +19,7 @@ def change_prompt(prompt, token, man):
     return prompt.replace(token, "woman with blue eyes")
 
 def find_face(start_img):
-  test_image = cv2.cvtColor(numpy.array(start_img), cv2.COLOR_RGB2BGR)
+  test_image = cv2.cvtColor(np.array(start_img), cv2.COLOR_RGB2BGR)
   grey = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
   face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
   faces = face_cascade.detectMultiScale(grey, 1.3, 5) # тут находим лицо
@@ -26,7 +27,7 @@ def find_face(start_img):
 
 def crop_face(image, faces, sec_num):
   faces_imgs = []
-  image = cv2.cvtColor(numpy.array(image), cv2.COLOR_RGB2BGR)
+  image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
   for (x, y, w, h) in faces:
     cropped = image[y-sec_num:y + h+sec_num, x-sec_num:x + w+sec_num]  # тут обрезаем лицо + делаем края по 20 с каждой стороны
     faces_imgs.append(cropped)
@@ -70,7 +71,7 @@ def merge(original, face, faces_info, size_of_cropped, sec_num):
 
 def resize_convert(pil_image, size_of_cropped):
   pil_image = pil_image.resize((size_of_cropped, size_of_cropped))  # без двойных скобок оно не работает!!!!
-  face = cv2.cvtColor(numpy.array(pil_image), cv2.COLOR_RGB2BGR)
+  face = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
   return face
 
 def image_grid(imgs, rows, cols):
@@ -80,6 +81,33 @@ def image_grid(imgs, rows, cols):
     for i, img in enumerate(imgs):
         grid.paste(img, box=(i % cols * w, i // cols * h))
     return grid
+
+def make_pyramid(face):
+  k = int(0.15*face.size[0])
+  width = face.size[0]
+  x = np.linspace(-1, 1, width)
+  y = np.linspace(-1, 1, width)
+  xv, yv = np.meshgrid(x,y)
+  f = abs(1 - np.maximum(abs(xv), abs(yv)))
+  сorner = f[k, k]
+  loc_max = f[int(width / 2), k - 1]
+  f = np.where(f >= сorner, 1, f/loc_max)
+  return f
+
+def make_gradient(img_orig, result, face, faces_info, secret_num):
+  ld_1 = img_orig.load()
+  ld_2 = result.load()
+  f = make_pyramid(face)
+  for x in range(face.size[0]):
+    for y in range(face.size[1]):
+      k = f[x, y]
+      color = []
+      im_x = faces_info[0,0]- secret_num + x
+      im_y = faces_info[0,1]- secret_num + y
+      for i in range(3):
+        color.append(int((1-k)*ld_1[im_x,im_y][i] + k*ld_2[im_x, im_y][i]))
+      ld_1[im_x,im_y] = tuple(color)
+  return img_orig
 
 pipe_inpaint = StableDiffusionInpaintPipeline.from_pretrained(
     "runwayml/stable-diffusion-inpainting", torch_dtype=torch.float16
